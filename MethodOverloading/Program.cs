0734691bdd7d17bd38533f4bb65065c466581d46
Alen Valdez <alenkvaldeztch@gmail.com>
2023-01-30 16:48:11 -0800

    methodoverloadex

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,30 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int num1, int num2)
         {
-            Console.WriteLine("Hello, World!");
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            if (isTrue && (num1 + num2) != 1)
+            {
+                return $"{num1 + num2} dollars";
+            }
+            else if (isTrue && (num1 + num2) == 1)
+            {
+                return $"{num1 + num2} dollar";           
+            }
+            else
+            {
+                return $"{num1 + num2}";
+            }
         }
     }
 }
